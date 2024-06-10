717e344cb4adc20ab58f8b6d7d0615345ddc6971
Edward <Edwardrafael2348@gmail.com>
2024-06-10 10:22:48 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    borderWIDTH: {
+      default: "20px",
+      1: "1px",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+    },
+  },
+  plugins: [],
+};
+
