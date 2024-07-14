6124127e428dfd7ae10bb663e2bfbc99ca230daf
Edward <Edwardrafael2348@gmail.com>
2024-07-14 20:12:55 +0700

    ganti nama

---
+++ assets/img/tutor_terminal/test.py
@@ -0,0 +1,18 @@
+
+def greeting(fname,lname,state):
+	print("Hello, %s %s! How is the weather in %s?" %(fname, lname, state))
+
+first_name = "Peter"
+last_name = "Gervase"
+mystate = "North Carolina"
+
+greeting(first_name, last_name, mystate)
+
+
+
+def input(
+		prompt: object ="",
+		/
+) -> str:
+	
+    input
